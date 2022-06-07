5f8724d7446b6e7dbeacb88f66d97321c29141e5
Matthew Rhoads <mematthew123@gmail.com>
2022-06-07 14:52:46 -0600

    set up supabase clinet and passed images

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,88 @@
-import Image from 'next/image';
+import Image from 'next/image'
+import { useState } from 'react'
+import { createClient } from '@supabase/supabase-js'
+import { DiffieHellman } from 'crypto'
 
 
 
-export default function Gallery() {
+
+
+export async function getStaticProps() {
+  const supabaseAdmin = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL || '',
+    process.env.SUPABASE_SERVICE_ROLE_KEY || ''
+  )
+
+  const { data } = await supabaseAdmin.from('Images').select('*').order('id')
+  return {
+    props: {
+      images: data,
+    },
+  }
+}
+
+function cn(...classes: string[]) {
+  return classes.filter(Boolean).join(' ')
+}
+
+type Image = {
+  id: number
+  href: string
+  imageSrc: string
+  name: string
+  username: string
+}
+
+export default function Gallery({ images }: { images: Image[] }) {
   return (
-    <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-      <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-        {/* Images will go here */}
-        <BlurImage />
+
+    <div className="container mx-auto font-bold font-2xl">
+      <div className="flex flex-wrap justify-center">
+        The Amazing Bernice Gallery
+        </div>
+      <div className="flex flex-wrap justify-center">
+        <div className = " text-gray-500 font-xl">
+          Pup of the stinkin' year 💛 Bernice
+        </div>
+      </div>
+
+      
+
+    <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+      <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+        {images.map((image) => (
+          <BlurImage key={image.id} image={image} />
+        ))}
       </div>
     </div>
-  );
+    </div>
+
+  )
 }
-function BlurImage() {
+
+function BlurImage({ image }: { image: Image }) {
+  const [isLoading, setLoading] = useState(true)
+
   return (
-    <a href="#" className="group">
-      <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
+    <a href={image.href} className="group">
+      <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
         <Image
           alt=""
-          src="https://bit.ly/placeholder-img"
+          src={image.imageSrc}
           layout="fill"
           objectFit="cover"
-          className="group-hover:opacity-75"
+          className={cn(
+            'duration-700 ease-in-out group-hover:opacity-75',
+            isLoading
+              ? 'scale-110 blur-2xl grayscale'
+              : 'scale-100 blur-0 grayscale-0'
+          )}
+          onLoadingComplete={() => setLoading(false)}
         />
       </div>
-      <h3 className="mt-4 text-sm text-gray-700"> Matthew Rhoads </h3>
-      <p className="mt-1 text-lg font-medium text-gray-900">@mematthew123</p>
+      <h3 className="mt-4 text-sm text-gray-700">{image.name}</h3>
+      <p className="mt-1 text-lg font-medium text-gray-900">{image.username}</p>
     </a>
-  );
+    
+  )
 }
